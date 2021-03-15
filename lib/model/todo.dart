56e14f2f6c48b285fc839214d7e1efdddb42495e
Edilson Moizinho <edilsondiasalves@outlook.com>
2021-03-14 22:59:36 -0300

    Creation of the model

---
+++ lib/model/todo.dart
@@ -0,0 +1,59 @@
+class Todo {
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+
+  Todo(this._title, this._priority, this._date, [this._description]);
+  Todo.withId(this._id, this._title, this._priority, this._date,
+      [this._description]);
+
+  int get id => this._id;
+  String get title => this._title;
+  String get description => this._description;
+  String get date => this._date;
+  int get priority => this._priority;
+
+  set title(String newTitle) {
+    if (newTitle.length <= 255) {
+      this._title = newTitle;
+    }
+  }
+
+  set description(String newDescription) {
+    if (newDescription.length <= 255) {
+      this._title = newDescription;
+    }
+  }
+
+  set priority(int newPriority) {
+    if (newPriority >= 0 && newPriority <= 3) {
+      this._priority = newPriority;
+    }
+  }
+
+  set date(String newDate) {
+    this._date = newDate;
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    map["title"] = this.title;
+    map["description"] = this.description;
+    map["date"] = this.date;
+    map["priority"] = this.priority;
+    if (this._id != null) {
+      map["id"] = this._id;
+    }
+    return map;
+  }
+
+  Todo.fromObject(dynamic o) {
+    this._id = o["id"];
+    this._title = o["title"];
+    this._description = o["description"];
+    this._date = o["date"];
+    this._priority = o["priority"];
+  }
+}
